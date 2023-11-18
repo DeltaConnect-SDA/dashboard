01eab115e2a7740d7a9f18237c1db70e44c59b01
yuke brilliant <yukebrilliant@gmail.com>
2023-11-19 04:56:24 +0700

    Feat: Plan status

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -1,9 +1,9 @@
 export enum Status {
-    WAITING = 1,
-    VERIFICATION = 2,
-    PROCESS = 3,
-    COMPLETE = 4,
-    CANCELED = 5,
-    DECLINED = 6
-  }
-  
+  WAITING = 1,
+  VERIFICATION = 2,
+  PROCESS = 3,
+  COMPLETE = 4,
+  CANCELED = 5,
+  DECLINED = 6,
+  PLAN = 7,
+}
